075ceae8d33247901b26cdf80ed53ecc147110c1
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-09-11 13:30:40 -0300

    versão 1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+// Opção de mudar o tema da página.
+function mudarCor () {
+    var corpo = document.getElementById('corpo');
+    corpo.classList.add('black');
+}
+
+function removeCor () {
+    var corpo = document.getElementById('corpo');
+    corpo.classList.remove('black');
+}
