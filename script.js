4eeeeaf7a869ff050c1b66efed898a3a6919616f
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-09-22 00:31:29 -0300

    1.2.4

--- script.js
+++ script.js
@@ -16,8 +16,20 @@ function autoContraste() {
 }
 /****************fim****************/
 
-// function teste () {
-//     var cap1 =  document.getElementById("saida") ;
-//     cap1.innerHTML = " <p>bchdbidbvdbvdhvdbvusevbsivbsdjvbdvbidjvbsduvjbdsivbesv</p>  <button>Sim</button>  <button>Não</button>";
-// }
-// teste();
+
+// Prologo do jogo!
+function prologo() {
+    let prologo = document.getElementById('saida');
+    prologo.innerHTML =  "<img src='https://pixeljoint.com/files/icons/full/map_animated.gif' alt='Imagem do rteino de hope of chaos'></img> <p>O reino de <strong>Hope</strong> costumava ser um reino de celebração, paz e prosperidade. Mas seu povo mal sabia que sua tranquilidade estava prestes a ter um fim. </p>  <button onclick='prologo1()'>Avançar</button>" ;
+
+}
+
+function prologo1() {
+    let prologo1 = document.getElementById('saida');
+    prologo1.innerHTML =  "<img src='arquivos/reino2.jpg'> <p>Como todo reino e seus mistérios, havia algo que rondava a imaginação e a curiosidade de seu povo: uma lenda de um suposto medalhão, de poder antigo e descochecido. Suspeitam eles que apenas a realeza possui maiores informações desta relíquia. Porém com o espalhar da lenda, a mesma atrai os olhos de desconhecido, cobertos de ganância e ódio direcionados ao reino. </p> <button onclick='prologo2()'>Avançar</button>";
+}
+
+function prologo2() {
+    let prologo3 = document.getElementById('saida');
+    prologo3.innerHTML = "<img src='arquivos/xinok.jpg'> <p>Em determinado dia, o reino se vê diante a uma grande ameaça. A sala onde se econtrava o medalhão foi brutalmente invadida. Dizem que essa sala era repleta de guardas rondando a relíquia, para garantir proteção. Mas todos eles foram expostos a um grande confronto com o invasor desconhecido. Supreendentemente, nenhum conseguiu resistir ao combate. A joia desejada, assim que possuida, expeliu tamanho poder que nem mesmo o invasor conseguiu suportar, Levando apenas duas partes desse objeto e deixando uma para trás. </p>" ;
+}
