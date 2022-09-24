75f16cd35c23db0e1524abdd135fa765245f4cb2
Guilherme <guilhermemascarenhas21g@gmail.com>
2022-09-24 02:25:03 -0300

    1.2.5

--- script.js
+++ script.js
@@ -30,6 +30,30 @@ function prologo1() {
 }
 
 function prologo2() {
-    let prologo3 = document.getElementById('saida');
-    prologo3.innerHTML = "<img src='arquivos/prologo3.png'> <p>Em determinado dia, o reino se vê diante a uma grande ameaça. A sala onde se econtrava o medalhão foi brutalmente invadida. Dizem que essa sala era repleta de guardas rondando a relíquia, para garantir proteção. Mas todos eles foram expostos a um grande confronto com o invasor desconhecido. Supreendentemente, nenhum conseguiu resistir ao combate. A joia desejada, assim que possuida, expeliu tamanho poder que nem mesmo o invasor conseguiu suportar, Levando apenas duas partes desse objeto e deixando uma para trás. </p>" ;
+    let prologo2 = document.getElementById('saida');
+    prologo2.innerHTML = "<img src='arquivos/prologo3.png'> <p>Em determinado dia, o reino se vê diante a uma grande ameaça. A sala onde se econtrava o medalhão foi brutalmente invadida. Dizem que essa sala era repleta de guardas rondando a relíquia, para garantir proteção. Mas todos eles foram expostos a um grande confronto com o invasor desconhecido. Supreendentemente, nenhum conseguiu resistir ao combate. A joia desejada, assim que possuida, expeliu tamanho poder que nem mesmo o invasor conseguiu suportar, Levando apenas duas partes desse objeto e deixando uma para trás. </p>  <button onclick='prologo3()'>Avançar</button>" ;
+}
+
+function prologo3 () {
+  let prologo3 = document.getElementById('saida');
+  prologo3.innerHTML = "<img src= 'arquivos/prologo3_1.jpg'> <p>Após acontecimento a polulação presencia estranhas mudanças em seu lar. Criaturas medievais e maquinas futuristicas passam a espalhar medo desespero a todos. O rei juntamente a outros de grande influência decidiram então criar um torneio, e que o vencedor final receberia a missão de ir em busca das peças restantes. Casom então, concluido a tafefa com glória, receberia a grande riqueza e alteração permanente de seu status. O novo herói da coroa e dos eu povo.</p> <button onclick='inicio()'>Avançar</button>" ;
+}
+// Fim do prólogo 
+
+
+// Inicio - hope of chaos 
+
+function inicio () {
+  let inicio = document.getElementById('saida');
+  inicio.innerHTML = "  <button onclick='inicio1()'>Adquirir baú </button> <button onclick='fim1()'>Negar baú</button>";
+}
+
+function fim1 () {
+   let fim1 =  document.getElementById('saida');
+   fim1.innerHTML = " <h3> Caminhamos até a passagem <span class='scd'>-secreta!</span></h3> <button> Avançar </button>";
+}
+
+function inicio1 () {
+    let inicio1 = document.getElementById('saida');
+    inicio.innerHTML = "<img src='arquivos/reino2.jpg'> <h3>Escolha-sua-<span class = 'scd'> classe:</span></h3>  <button>Guerreiro</button> <button>Arqueiro</button> <button>Mago</button>" ;
 }
